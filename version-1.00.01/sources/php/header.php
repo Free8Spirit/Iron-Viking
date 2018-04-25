fedffe3f8488676595a72fa9c595d17d2204f545
Free8Spirit <38473227+Free8Spirit@users.noreply.github.com>
2018-04-24 22:46:02 -0500

    php

---
+++ version-1.00.01/sources/php/header.php
@@ -0,0 +1,40 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+    <!-- Shared File: Dakota Colton and David Vert Co-Owners/editors of index.html -->
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="../css/styles.css" />
+        <title> Iron Viking </title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="main-wrapper">
+                    <ul>
+                        <li>
+                            <a href="index.html"> Home </a>
+                        </li>
+                    </ul>
+                    <div class="nav-login">
+						<?php
+							if (isset($SESSION['u_uid'])) {
+								echo '<form action="logout.inc.php" method="POST">
+									<button type="submit" name="submit"> Logout </button>
+									</form>';
+							} else {
+								<form action="login.inc.php" method="POST">
+									<input type="text" name="uid" placeholder="Username/e-mail" />
+									<input type="password" name="pwd" placeholder="password" />
+									<button type="submit" name="submit"> Login </button>
+								</form>
+								<a href="signup.php"> Sign up </a>
+							}
+						?>
+                    </div>
+                </div>
+            </nav>
+        </header>
